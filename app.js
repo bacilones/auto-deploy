346ffaa5f3c3d315d2360d57fe110e552444aed7
Gonzalo Bahamondez <gonzalo@becual.com>
2016-11-26 13:31:52 -0300

    adding pm2 restart

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const logger = require('morgan');
 
 const projectPath  = process.env.PROJECT_PATH;
 const port = process.env.PORT;
+const appName = process.env.APP_NAME;
 
 console.log('project path --> ', projectPath);
 console.log('port --> ', port);
+console.log('app name -->', appName);
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +19,7 @@ app.use(bodyParser.json());
 app.post('/update', function(req, res) {
     if (req.body.ref === 'refs/heads/release') {
         console.log('Updating repo ... !');
-        var child = exec(`cd ${projectPath} && git pull origin release`, function(err, execution) {
+        var child = exec(`cd ${projectPath} && git pull origin release && pm2 restart ${appName}`, function(err, execution) {
             if (err) return res.status(500).send();
         });
     }
