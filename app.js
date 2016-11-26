804efb62385d1d291038f2d73858d05a3f095032
Gonzalo Bahamondez <gonzalo@becual.com>
2016-11-26 13:46:58 -0300

    adding front end flow

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const logger = require('morgan');
 const projectPath  = process.env.PROJECT_PATH;
 const port = process.env.PORT;
 const appName = process.env.APP_NAME;
+const appType = process.env.APP_TYPE;
 
 console.log('project path --> ', projectPath);
 console.log('port --> ', port);
 console.log('app name -->', appName);
+console.log('app type -->', appType);
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,10 +20,19 @@ app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
     if (req.body.ref === 'refs/heads/release') {
-        console.log('Updating repo ... !');
-        var child = exec(`cd ${projectPath} && git pull origin release && pm2 restart ${appName}`, function(err, execution) {
-            if (err) return res.status(500).send();
-        });
+
+        if (appType === 'back') {
+            console.log('Updating backend app ...');
+            var child = exec(`cd ${projectPath} && git pull origin release && npm install && pm2 restart ${appName}`, function(err, execution) {
+                if (err) return res.status(500).send();
+            });
+        }
+        else if (appType == 'front') {
+            console.log('Updating front end app ...');
+            var child = exec(`cd ${projectPath} && git pull origin release`, function(err, execution) {
+                if (err) return res.status(500).send();
+            });
+        }
     }
     res.send('OK');
 });
