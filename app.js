f58aaa942c987fbf1f5cf19431389f422541b637
Gonzalo Bahamondez <gonzalo@becual.com>
2016-11-26 12:58:55 -0300

    adding config files

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const exec    = require('child_process').exec;
-
-
+const  bodyParser = require('body-parser');
 const app = express();
 
 var projectPath  = process.env.PROJECT_PATH;
 
-app.get('/update', function() {
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.post('/update', function() {
     var child = exec(`cd ${projectPath} && git pull origin release`, function(err, res) {
         if (err) return res.status(500).send();
         res.send('OK');
