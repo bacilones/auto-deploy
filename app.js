bd86c1e079598abcf26ff5d1b23bf167ed03b074
Gonzalo Bahamondez <gonzalo@becual.com>
2016-11-26 13:01:40 -0300

    debuging body

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var projectPath  = process.env.PROJECT_PATH;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-app.post('/update', function() {
+app.post('/update', function(req, res) {
+    console.log('body --> ', req.body);
+
     var child = exec(`cd ${projectPath} && git pull origin release`, function(err, res) {
         if (err) return res.status(500).send();
         res.send('OK');
